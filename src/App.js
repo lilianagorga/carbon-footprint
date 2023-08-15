adbeb77f4cab579cb1beb225eafe90d3308e066a
Liliana Gorga <gorgaliliana87@gmail.com>
2023-08-15 17:05:59 +0200

    remove non necessary Section page e switch Links where in Section to Home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Home from './pages/Home';
-import Section from './pages/Section';
 import Countries from './components/Countries';
 import Average from './components/Average';
 import Geo from './components/Geo';
@@ -13,14 +12,10 @@ function App() {
             <li>
               <Link to="/">Home</Link>
             </li>
-            <li>
-              <Link to="/section">Section</Link>
-            </li>
           </ul>
         </nav>
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route exact path="/section" element={<Section />} />
           <Route exact path="/countries" element={<Countries />} />
           <Route exact path="/average" element={<Average />} />
           <Route exact path="/geo" element={<Geo />} />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
+      <div>
+        <Link to="/average">CO2 by Year</Link>
+      </div>
+      <div>
+        <Link to="/countries">CO2 by Country</Link>
+      </div>
     </div>
   );
 };
