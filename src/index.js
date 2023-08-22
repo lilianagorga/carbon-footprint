6c37a8764ca4f487198edde14c45475fa35dcce1
Liliana Gorga <gorgaliliana87@gmail.com>
2023-08-22 15:11:05 +0200

    add React Query to avoid double API call

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      {process.env.NODE_ENV === 'development' && <ReactQueryDevtools />}
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
--- src/pages/Emissions.js
+++ src/pages/Emissions.js
@@ -1,41 +1,47 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useQuery } from 'react-query';
 import axios from 'axios';
 import Chart from '../components/chart/Chart';
 import { sortAndFormatData, filterDataByDateRange } from '../utils';
 
+const fetchData = async ({ country, start, end }) => {
+  const currentDate = new Date().toJSON();
+  const startDate = '2019-01-01';
+  const url = `https://api.v2.emissions-api.org/api/v2/carbonmonoxide/average.json?country=${country}&begin=${startDate}&end=${currentDate}&offset=0`;
+
+  const response = await axios.get(url);
+  const data = response.data;
+  const formattedData = sortAndFormatData(data);
+  const filterRange = filterDataByDateRange(formattedData, start, end);
+
+  const average = filterRange.reduce((acc, curr) => acc + curr.average, 0).toFixed(2);
+  console.log('avg', average);
+
+  return {
+    rangeEmissions: filterRange,
+    average: average,
+  };
+};
+
 const Emissions = () => {
   const { country, start, end } = useParams();
-  const [rangeEmissions, setRangeEmissions] = useState([]);
-  const [average, setAverage] = useState(0);
 
-  useEffect(() => {
-      const currentDate = new Date().toJSON();
-      const startDate = '2019-01-01';
-      const url = `https://api.v2.emissions-api.org/api/v2/carbonmonoxide/average.json?country=${country}&begin=${startDate}&end=${currentDate}&offset=0`;
+  const { data, error, isLoading } = useQuery(
+    ['emissions', { country, start, end }],
+    () => fetchData({ country, start, end })
+  );
 
-      const fetchData = async () => {
-        try {
-          const response = await axios.get(url);
-          const data = response.data;
-          const formattedData = sortAndFormatData(data);
-          const filterRange = filterDataByDateRange(formattedData, start, end);
-          setRangeEmissions(filterRange);
-          const avg = filterRange.reduce((acc, curr) => acc + curr.average, 0).toFixed(2);
-          setAverage(avg);
-          console.log('avg', avg);
-        } catch (error) {
-          console.log(error);
-        }
-      };
-    
-      fetchData();
-    }, [country, start, end]);
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
-    return (
-      <Chart rangeEmissions={rangeEmissions} average={average} />
-    );
-  };
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  return <Chart rangeEmissions={data.rangeEmissions} average={data.average} />;
+};
   
   export default Emissions;
 
