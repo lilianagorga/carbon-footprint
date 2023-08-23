0475737b8ff4af4cd76eecf956fdbb1c71f1e3f9
Liliana Gorga <gorgaliliana87@gmail.com>
2023-08-23 18:19:42 +0200

    add chart for selected period

--- src/pages/Emissions.js
+++ src/pages/Emissions.js
@@ -1,17 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import Chart from '../components/chart/Chart';
+import Chart from '../components/charts/Chart';
+import PromptChart from '../components/charts/PromptChart';
 import { sortAndFormatData, filterDataByDateRange } from '../utils';
 
 const Emissions = () => {
   const { country, latitude, longitude, start, end } = useParams();
   const [rangeEmissions, setRangeEmissions] = useState([]);
   const [average, setAverage] = useState(0);
+  const [promptChart, setPromptChart] = useState([]);
+  const [selectedPeriod, setSelectedPeriod] = useState('whole');
 
   useEffect(() => {
-    const currentDate = new Date().toJSON();
-    const startDate = '2019-01-01';
+    const currentDate = new Date().toJSON().split('T')[0];
+    const startDate = getPeriod(selectedPeriod);
     let url;
 
     if (latitude && longitude) {
@@ -29,20 +32,67 @@ const Emissions = () => {
         setRangeEmissions(filterRange);
         const avg = filterRange.reduce((acc, curr) => acc + curr.average, 0).toFixed(2);
         setAverage(avg);
+        const additionalFilterRange = filterDataByDateRange(formattedData, getPeriod(selectedPeriod), end);
+        setPromptChart(additionalFilterRange);
         console.log('avg', avg);
       } catch (error) {
         console.log(error);
       }
     };
     fetchData();
-  }, [country, latitude, longitude, start, end]);
+  }, [country, latitude, longitude, start, end, selectedPeriod]);
+  const getPeriod = (period) => {
+    const currentDate = new Date();
+    switch (period) {
+      case 'whole':
+        return '2019-01-01';
+      case 'last6':
+        const sixMonthsAgo = new Date(currentDate);
+        sixMonthsAgo.setMonth(currentDate.getMonth() - 6);
+        return sixMonthsAgo.toISOString().split('T')[0];
+      case 'last3':
+        const threeMonthsAgo = new Date(currentDate);
+        threeMonthsAgo.setMonth(currentDate.getMonth() - 3);
+        return threeMonthsAgo.toISOString().split('T')[0];
+      case 'last1':
+        const oneMonthAgo = new Date(currentDate);
+        oneMonthAgo.setMonth(currentDate.getMonth() - 1);
+        return oneMonthAgo.toISOString().split('T')[0];
+      case 'last12':
+        const oneYearAgo = new Date(currentDate);
+        oneYearAgo.setFullYear(currentDate.getFullYear() - 1);
+        return oneYearAgo.toISOString().split('T')[0];
+      default:
+        return '2019-01-01';
+    }
+  }; 
+
+  const handleDate = (e) => {
+    setSelectedPeriod(e.target.value);
+  };
 
   return (
     <div>
-      <Chart rangeEmissions={rangeEmissions} average={average} />
+      <div>
+        <label>
+          Period:
+          <select value={selectedPeriod} onChange={handleDate}>
+            <option value="whole">Whole Period</option>
+            <option value="last12">Last One Year</option>
+            <option value="last6">Last 6 Months</option>
+            <option value="last3">Last 3 Months</option>
+            <option value="last1">Last Month</option>
+          </select>
+        </label>
+      </div>
+      <div className="charts-container">
+        <Chart rangeEmissions={rangeEmissions} average={average} />
+        <PromptChart data={promptChart} />
+      </div>
     </div>
   );
 };
   
   export default Emissions;
+
 
