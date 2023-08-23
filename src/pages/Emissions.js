2663056e0782d088bc8affdbd601c2918ea58668
Liliana Gorga <gorgaliliana87@gmail.com>
2023-08-23 08:46:15 +0200

    remove comments

--- src/pages/Emissions.js
+++ src/pages/Emissions.js
@@ -4,39 +4,6 @@ import axios from 'axios';
 import Chart from '../components/chart/Chart';
 import { sortAndFormatData, filterDataByDateRange } from '../utils';
 
-// const Emissions = () => {
-//   const { country, start, end } = useParams();
-//   const [rangeEmissions, setRangeEmissions] = useState([]);
-//   const [average, setAverage] = useState(0);
-
-//   useEffect(() => {
-//       const currentDate = new Date().toJSON();
-//       const startDate = '2019-01-01';
-//       const url = `https://api.v2.emissions-api.org/api/v2/carbonmonoxide/average.json?country=${country}&begin=${startDate}&end=${currentDate}&offset=0`;
-
-//       const fetchData = async () => {
-//         try {
-//           const response = await axios.get(url);
-//           const data = response.data;
-//           const formattedData = sortAndFormatData(data);
-//           const filterRange = filterDataByDateRange(formattedData, start, end);
-//           setRangeEmissions(filterRange);
-//           const avg = filterRange.reduce((acc, curr) => acc + curr.average, 0).toFixed(2);
-//           setAverage(avg);
-//           console.log('avg', avg);
-//         } catch (error) {
-//           console.log(error);
-//         }
-//       };
-    
-//       fetchData();
-//     }, [country, start, end]);
-
-//     return (
-//       <Chart rangeEmissions={rangeEmissions} average={average} />
-//     );
-//   };
-
 const Emissions = () => {
   const { country, latitude, longitude, start, end } = useParams();
   const [rangeEmissions, setRangeEmissions] = useState([]);
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -3,44 +3,6 @@ import { useNavigate } from 'react-router-dom';
 import { getAllCountries } from '../utils';
 import Modal from '../components/modal/Modal';
 
-// const Search = () => {
-//   const [country, setCountry] = useState('');
-//   const [startDate, setStartDate] = useState('');
-//   const [endDate, setEndDate] = useState('');
-//   const [countries, setCountries] = useState([]);
-//   const navigate = useNavigate();
-
-//   useEffect(() => {
-//     const fetchCountries = async () => {
-//       try {
-//         const countries = await getAllCountries();
-//         setCountries(countries);
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     };
-
-//     fetchCountries();
-//   }, []);
-
-//   const handleSubmit = () => {
-//     navigate(`/emissions/${country}/${startDate}/${endDate}`);
-//   };
-
-//   return (
-//     <Modal
-//       country={country}
-//       countries={countries}
-//       startDate={startDate}
-//       endDate={endDate}
-//       setCountry={setCountry}
-//       setStartDate={setStartDate}
-//       setEndDate={setEndDate}
-//       handleSubmit={handleSubmit}
-//     />
-//   );
-// };
-
 const Search = () => {
   const [country, setCountry] = useState('');
   const [startDate, setStartDate] = useState('');
