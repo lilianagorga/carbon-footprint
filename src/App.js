48d90b8c82f7cf7c6b8fa75905f1d84c291a0095
Liliana Gorga <gorgaliliana87@gmail.com>
2023-08-24 17:23:57 +0200

    add about page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Search from './pages/Search';
 import Emissions from './pages/Emissions';
+import About from './pages/About';
 import Countries from './components/Countries';
 
 function App() {
@@ -19,12 +20,16 @@ function App() {
           <li>
             <Link to="/countries">Countries</Link>
           </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
         </ul>
       </nav>
 
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/search" element={<Search/>} />
+          <Route path="/about" element={<About />} />
           <Route path="/emissions/country/:country/:start/:end" element={<Emissions />} />
           <Route path="/emissions/coordinates/:latitude/:longitude/:start/:end" element={<Emissions />} />
           <Route path="/countries" element={<Countries />} />
