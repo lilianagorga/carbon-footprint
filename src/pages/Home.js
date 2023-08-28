fdb80f228c8481df7f93ffc9000e948441ed0181
Liliana Gorga <gorgaliliana87@gmail.com>
2023-08-28 17:09:07 +0200

    Add video-nasa-1.mp4 using Git LFS

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import '../assets/styles/home.css';
 import imageNasa1 from '../assets/img/img-nasa-1.jpeg';
 import imageNasa2 from '../assets/img/img-nasa-2.jpeg';
+import videoNasa1 from '../assets/video/video-nasa-1.mp4';
 
 const Home = () => {
   return (
@@ -35,7 +36,19 @@ const Home = () => {
             Global atmospheric carbon dioxide concentrations as recorded by NASA's Orbiting Carbon Observatory-2 from October 1 to November 11. The measurements reveal the dynamic behavior of carbon dioxide in Earth's atmosphere, offering insights into its distribution and variation.
           </p>
         </div>
-        
+        <div className='video'>
+          <video controls>
+            <source src={videoNasa1} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+          <p>
+            Carbon dioxide plays a crucial role in the Earth's climate system, contributing to the retention of heat in the atmosphere. This gas is released from various human activities, such as the burning of fossil fuels, and its concentrations fluctuate seasonally.
+
+            Using data collected from NASA's Orbiting Carbon Observatory (OCO-2) satellite, scientists have developed a model that simulates the behavior of carbon in the atmosphere between September 1, 2014, and August 31, 2015.
+
+            These models enhance our understanding of carbon dynamics and enable predictions about regions with high or low carbon dioxide concentrations based on terrestrial activities.
+          </p>
+        </div>
       </div>
       <p className="conclusion">
         We believe that understanding the impact of our actions is the first step toward a sustainable future. We're here to support you in your journey toward a more eco-friendly life. Thank you for being part of the change! 
