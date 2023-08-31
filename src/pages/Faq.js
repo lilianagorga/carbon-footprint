a828c1de23e67c3002bf372107395122dfb3bf9a
Liliana Gorga <gorgaliliana87@gmail.com>
2023-08-31 19:49:48 +0200

    move media array outside Faq page

--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -1,40 +1,11 @@
 import React from 'react';
-import imageNasa1 from '../assets/img/img-nasa-1.jpeg';
-import imageNasa2 from '../assets/img/img-nasa-2.jpeg';
-import imageNasa3 from '../assets/img/img-nasa-3.jpeg';
 import '../assets/styles/faq.scss';
 import MediaGroup from '../components/FaqGroups/MediaGroup';
 import { FaqGroup } from '../components/FaqGroups/FaqGroup';
 import { faqArray } from '../components/FaqArrays/FaqArray';
+import { mediaArray } from '../components/FaqArrays/MediaArray';
 
 const Faq = () => {
-  const mediaArray = [
-    {
-      type: 'main',
-      content: [
-        {
-          source: imageNasa1,
-          alt: 'Global CO2 Levels',
-          description: "This illustration depicts how carbon is exchanged within Mars, involving its interior, surface rocks, polar caps, waters, and atmosphere. It also highlights a mechanism through which carbon is lost from the Martian atmosphere, affecting the isotopic ratio. Carbon dioxide (CO2) in Mars' atmosphere originates from the planet's mantle, released through volcanic activity or trapped in rocks crystallized from magma and later released. Upon entering the atmosphere, CO2 interacts with polar caps, transitioning from a gaseous to solid state. It can also dissolve in Martian waters, forming solid carbonates. The illustration portrays the loss of CO2 into space, mainly through ultraviolet photodissociation. This process breaks down CO2 molecules, releasing carbon atoms and influencing isotopic ratios. These complex interactions provide insights into the atmospheric dynamics of Mars."
-        }
-      ],
-    },
-    {
-      type: 'aside',
-      content: [
-        {
-          source: imageNasa2,
-          alt: 'Carbon-Dioxide Snowfall on Mars',
-          description: "NASA's Mars Reconnaissance Orbiter has captured striking evidence of carbon-dioxide snow clouds enveloping Mars, along with the astonishing phenomenon of carbon-dioxide snow descending to the Martian surface. These remarkable observations provide valuable insights into the planet's atmospheric intricacies and seasonal dynamics, showcasing the unique interplay between atmospheric components on the Red Planet.This discovery sheds light on the complex climate dynamics of the Red Planet and how its atmosphere interacts with the surface. The presence of snowfall composed of carbon dioxide adds another intriguing layer to our understanding of Mars' ever-evolving environment."
-        },
-        {
-          source: imageNasa3,
-          alt: 'Effect of Seasonal Vegetation Cycle on Global Atmospheric Carbon Dioxide',
-          description: "Exploring the Influence of Seasonal Vegetation Cycle on Fluctuations in Global Atmospheric Carbon Dioxide Levels. This image delves into the intricate relationship between Earth's seasonal vegetation cycle and the dynamics of atmospheric carbon dioxide concentrations. By correlating vegetation changes with CO2 levels, scientists gain insights into how natural processes impact our planet's carbon balance throughout the year. This deeper understanding contributes to the ongoing efforts to mitigate and address climate change, fostering a more sustainable future",
-        }
-      ],
-    },
-  ];
 
   return (
     <div className='page-container'>
